213607000d961c404b5697f951668a37a6645b6c
ReactifyStudio <winchygichu@gmail.com>
2021-02-09 03:47:15 +0300

    Login and Register Page Fixes...

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -2,21 +2,31 @@ import React from 'react';
 import {useForm} from 'react-hook-form';
 
 function RegisterPage(){
-    const { register, handleSubmit } = useForm();
+    const { register, errors, handleSubmit } = useForm();
     function saveData(){
         console.log(1)
     }
     return(
         <form onSubmit={handleSubmit(data => saveData(data))}>
             <label>First Name</label>
-            <input name="name" type="text" placeholder="Your first name" ref={register} />
+            <input name="Fname" type="text" 
+              placeholder="Your first name" 
+              ref={register({ required: true })} 
+            />
+            {errors.Fname && "Required"}
 
             <label htmlFor="">Last Name</label>
-            <input type="text" placeholder="Your last name"/>
-
+            <input type="text" name="Lname" 
+            placeholder="Your last name" ref={register({ required: true })}
+            />
+            {errors.Lname && "Required"}
 
             <label htmlFor="">Email</label>
-            <input type="email" placeholder="Enter Email"/>
+            <input type="email" name="email" 
+            placeholder="Enter Email"
+            ref={register({ required: true })}
+            />
+            {errors.email && "Required"}
 
             <label htmlFor="">Role</label>
             <select name="" id="">
@@ -25,16 +35,31 @@ function RegisterPage(){
             </select>
 
             <label htmlFor="">National ID</label>
-            <input type="number" name="" id="" placeholder="ID Number"/>
+            <input type="number" name="id" id="" 
+            placeholder="ID Number"
+            ref={register({ required: true })}
+            />
+            {errors.id && "Required"}
 
             <label htmlFor="">Phone Number</label>
-            <input type="tel" name="" id="" placeholder="E.g, 07....."/>
+            <input type="tel" name="phone" id="" 
+            placeholder="E.g, 07....."
+            ref={register({ required: true })}
+            />
+            {errors.phone && "Required"}
 
             <label>Enter Password</label>
-            <input type="password" name="" id="" placeholder="Enter Password"/>
-
+            <input type="password" name="password" id="" 
+            placeholder="Enter Password"
+            ref={register({ required: true })}
+            />
+            {errors.password && "Required"}
             <label htmlFor="">Confirm Password</label>
-            <input type="password" name="" id="" placeholder="Confirm Password Password"/>
+            <input type="password" name="confPassword" id="" 
+            placeholder="Confirm Password Password"
+            ref={register({ required: true })}
+            />
+            {errors.confPassword && "Required"}
 
             <input type="submit" /> 
         </form>
