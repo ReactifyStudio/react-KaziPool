6cf8fc5e25cecab43f990001ef3b7c39bbc5f485
ReactifyStudio <winchygichu@gmail.com>
2021-02-21 18:56:23 +0300

    :rocket:

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React,{Fragment} from 'react';
 import Header from '../components/Header';
+import Jumbotron from '../components/Jumbotron';
 import JobCard from '../components/JobCard'
 import HowItWorks from '../components/HowItWorks';
 import Footer from '../components/Footer';
@@ -8,6 +9,11 @@ function HomePage() {
   return (
     <Fragment>
       <Header/>
+
+      <h1>SECTION FOR SHOWCASE IMAGE ETC..</h1>
+      <p>dev in progress</p>
+      <Jumbotron/>
+
       <JobCard/>
       <HowItWorks/>
       <Footer/>        
