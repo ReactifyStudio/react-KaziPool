67ab74b078ff9c47609111362f688c29fa9727e2
ReactifyStudio <winchygichu@gmail.com>
2021-02-24 01:35:12 +0300

    edited

--- src/dev/BidList.jsx
+++ src/dev/BidList.jsx
@@ -31,7 +31,7 @@ class BidList extends React.Component {
 <div className='container py-4'>
 <div className='row justify-content-center'>
   <div className='col-md-8'>
-    <div className='card'>
+    <div className='card dash'>
       <div className='card-header'>Bid List</div>
       <div className='card-body'>
         <ul className='list-group list-group-flush'>
