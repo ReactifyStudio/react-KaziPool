8dc7f891f7a75aed2b66cf071dd1a61bc1769ca3
ReactifyStudio <winchygichu@gmail.com>
2021-02-23 16:57:10 +0300

    minor edits

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,12 @@
 import React,{Fragment} from 'react';
-// import Header from '../components/Header';
-import Jumbotron from '../components/Jumbotron';
+// import Jumbotron from '../components/Jumbotron';
 import JobCard from '../components/JobCard'
-import HowItWorks from '../components/HowItWorks';
+
 import Footer from '../components/Footer';
 
 function HomePage() {
   return (
     <Fragment>
-      {/* <Header/> */}
     <center>
       <h1 style={{fontSize:'3.4rem'}}>IN PROGRESS</h1>
 
@@ -16,7 +14,7 @@ function HomePage() {
       {/* <Jumbotron/> */}
      
       <JobCard/>
-      <HowItWorks/>
+
       <Footer/>        
     </Fragment>
   );
