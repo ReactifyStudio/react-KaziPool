13062e4ae35659c73bf71b50d3e4fa6ed72eb295
ReactifyStudio <winchygichu@gmail.com>
2021-02-23 23:03:16 +0300

    minor edits

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,14 @@
 import React,{Fragment} from 'react';
 // import Jumbotron from '../components/Jumbotron';
-import JobCard from '../components/JobCard'
+import JobCard from '../components/JobCard';
+import Header from '../components/Header'
 
 import Footer from '../components/Footer';
 
 function HomePage() {
   return (
     <Fragment>
+      <Header/>
       {/* <Jumbotron/> */}
       <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
       <h1>Trending Jobs <i class="fas fa-fire"></i> </h1>
