ad5aca27e5245ec42a16324b28fda30c00f8008e
ReactifyStudio <winchygichu@gmail.com>
2021-02-23 20:06:47 +0300

    dev branch :rocket:

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,9 @@ import F04 from './components/F04'
 import BasicDashboard from './components/BasicDashboard';
 
 
+import Dev from './dev/Dev'
+
+
 function App() {
   return (
     <Fragment>
@@ -29,6 +32,7 @@ function App() {
         <Route exact path='/register' component={RegisterPage}/>
         <Route exact path='/login' component={LoginPage}/>
         <Route exact path='*' component={F04}/>
+        <Route exact path='/dev' component={Dev}/>
 
       </Switch>  
       </Router>       
--- src/dev/Dev.jsx
+++ src/dev/Dev.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import AssignBid from './AssignBid'
+import AssignedBids from './AssignedBids'
+import BidList from './BidList'
+import ClientValidate from './ClientValidate'
+import NewJob from './NewJob'
+import ListDetails from './ListDetails'
+import NewListing from './NewListing'
+import ViewJobs from './ViewJobs'
+import ViewListings from './ViewListings'
 
 
 function Dev() {
     return (
         <div>
+            <AssignBid/>
+            <AssignedBids/>
+            <BidList/>
+            <ClientValidate/>
+            <ListDetails/>
+            <NewJob/>
+            <NewListing/>
+            <ViewJobs/>
+            <ViewListings/>
             
         </div>
     )
