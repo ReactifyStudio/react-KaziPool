ee81fa05e47b4cca1dc16c89ff2b9369d06b95be
ReactifyStudio <winchygichu@gmail.com>
2021-02-24 01:42:36 +0300

    edited

--- src/dev/ListDetails.jsx
+++ src/dev/ListDetails.jsx
@@ -41,7 +41,7 @@ class ListDetails extends React.Component {
 <div className='container py-4'>
 <div className='row justify-content-center'>
   <div className='col-md-8'>
-    <div className='card'>
+    <div className='card dash'>
       <div className='card-header'><h2>Job Details | List </h2></div>
       <div className='card-body'>
         <ul className='list-group list-group-flush'>
