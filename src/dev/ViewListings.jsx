6c5216abde674fd4bca00ee11303c2f629c171c1
ReactifyStudio <winchygichu@gmail.com>
2021-02-24 01:37:42 +0300

    edited

--- src/dev/ViewListings.jsx
+++ src/dev/ViewListings.jsx
@@ -28,7 +28,7 @@ class ViewListings extends React.Component {
 <div className='row justify-content-center'>
   <div className='col-md-8'>
     <div className='card dash'>
-      <div className='card-header'>View Jobs | Listings</div>
+      <div className='card-header'><h2>View Jobs | Listings</h2></div>
       <div className='card-body'>
         <ul className='list-group list-group-flush'>
         {listings.map(list => (
